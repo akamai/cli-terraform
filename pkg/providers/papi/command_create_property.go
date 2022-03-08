e59637b0893ad066505521e848864767e56267ca
Michal Wojcik <miwojci@akamai.com>
2022-03-08 11:36:44 +0000

    DXE-308 Replace v1 of akamaiopen-edgegrid-golang with v2 in papi package

--- pkg/providers/papi/command_create_property.go
+++ pkg/providers/papi/command_create_property.go
@@ -17,18 +17,20 @@
 package papi
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"text/template"
 
-	"github.com/akamai/AkamaiOPEN-edgegrid-golang/edgegrid"
-	"github.com/akamai/AkamaiOPEN-edgegrid-golang/papi-v1"
-	"github.com/akamai/cli-terraform/pkg/providers/papi/hapi"
+	"github.com/akamai/AkamaiOPEN-edgegrid-golang/v2/pkg/hapi"
+	"github.com/akamai/AkamaiOPEN-edgegrid-golang/v2/pkg/papi"
+	"github.com/akamai/cli-terraform/pkg/edgegrid"
 	"github.com/akamai/cli-terraform/pkg/tools"
 	"github.com/akamai/cli/pkg/terminal"
 	"github.com/fatih/color"
@@ -76,68 +78,57 @@ type TFData struct {
 
 // RulesTemplate represent data used for rules
 type RulesTemplate struct {
-	AccountID       string             `json:"accountId"`
-	ContractID      string             `json:"contractId"`
-	GroupID         string             `json:"groupId"`
-	PropertyID      string             `json:"propertyId"`
-	PropertyVersion int                `json:"propertyVersion"`
-	Etag            string             `json:"etag"`
-	RuleFormat      string             `json:"ruleFormat"`
-	Rule            *RuleTemplate      `json:"rules"`
-	Errors          []*papi.RuleErrors `json:"errors,omitempty"`
+	AccountID       string        `json:"accountId"`
+	ContractID      string        `json:"contractId"`
+	GroupID         string        `json:"groupId"`
+	PropertyID      string        `json:"propertyId"`
+	PropertyVersion int           `json:"propertyVersion"`
+	Etag            string        `json:"etag"`
+	RuleFormat      string        `json:"ruleFormat"`
+	Rule            *RuleTemplate `json:"rules"`
+	Errors          []*papi.Error `json:"errors,omitempty"`
 }
 
 // RuleTemplate represent data used for single rule
 type RuleTemplate struct {
-	Name                string                            `json:"name"`
-	Criteria            []*papi.Criteria                  `json:"criteria,omitempty"`
-	Behaviors           []*papi.Behavior                  `json:"behaviors,omitempty"`
-	Children            []string                          `json:"children,omitempty"`
-	Comments            string                            `json:"comments,omitempty"`
-	CriteriaLocked      bool                              `json:"criteriaLocked,omitempty"`
-	CriteriaMustSatisfy papi.RuleCriteriaMustSatisfyValue `json:"criteriaMustSatisfy,omitempty"`
-	UUID                string                            `json:"uuid,omitempty"`
-	Variables           []*papi.Variable                  `json:"variables,omitempty"`
-	AdvancedOverride    string                            `json:"advancedOverride,omitempty"`
+	Name                string                       `json:"name"`
+	Criteria            []papi.RuleBehavior          `json:"criteria,omitempty"`
+	Behaviors           []papi.RuleBehavior          `json:"behaviors,omitempty"`
+	Children            []string                     `json:"children,omitempty"`
+	Comments            string                       `json:"comments,omitempty"`
+	CriteriaLocked      bool                         `json:"criteriaLocked,omitempty"`
+	CriteriaMustSatisfy papi.RuleCriteriaMustSatisfy `json:"criteriaMustSatisfy,omitempty"`
+	UUID                string                       `json:"uuid,omitempty"`
+	Variables           []papi.RuleVariable          `json:"variables,omitempty"`
+	AdvancedOverride    string                       `json:"advancedOverride,omitempty"`
 
 	Options struct {
 		IsSecure bool `json:"is_secure,omitempty"`
 	} `json:"options,omitempty"`
 
-	CustomOverride *papi.CustomOverride `json:"customOverride,omitempty"`
-}
-
-func getEdgegridConfig(c *cli.Context) (edgegrid.Config, error) {
-	config, err := edgegrid.Init(c.String("edgerc"), c.String("section"))
-	if err != nil {
-		return edgegrid.Config{}, cli.Exit(err.Error(), 1)
-	}
-
-	if len(c.String("accountkey")) > 0 {
-		config.AccountKey = c.String("accountkey")
-	} else if len(c.String("account-key")) > 0 {
-		config.AccountKey = c.String("account-key")
-	}
-
-	return config, nil
+	CustomOverride *papi.RuleCustomOverride `json:"customOverride,omitempty"`
 }
 
 // CmdCreateProperty is an entrypoint to create-property command
 func CmdCreateProperty(c *cli.Context) error {
-	term := terminal.Get(c.Context)
-
+	ctx := c.Context
 	log.SetOutput(ioutil.Discard)
-	if c.NArg() == 0 {
-		cli.ShowCommandHelp(c, c.Command.Name)
+	if c.NArg() < 2 {
+		if err := cli.ShowCommandHelp(c, c.Command.Name); err != nil {
+			return cli.Exit(color.RedString("Error displaying help command"), 1)
+		}
 		return cli.Exit(color.RedString("property name is required"), 1)
 	}
 
+	sess := edgegrid.GetSession(c.Context)
+	client := papi.Client(sess)
+	clientHapi := hapi.Client(sess)
 	if c.IsSet("tfworkpath") {
 		tools.TFWorkPath = c.String("tfworkpath")
 	}
 	tools.TFWorkPath = filepath.FromSlash(tools.TFWorkPath)
 	if stat, err := os.Stat(tools.TFWorkPath); err != nil || !stat.IsDir() {
-		return cli.NewExitError(color.RedString("Destination work path is not accessible."), 1)
+		return cli.Exit(color.RedString("Destination work path is not accessible"), 1)
 	}
 	err := tools.CheckFiles(
 		tools.CreateTFFilename("property"),
@@ -149,15 +140,6 @@ func CmdCreateProperty(c *cli.Context) error {
 		return cli.Exit(err, 1)
 	}
 
-	config, err := getEdgegridConfig(c)
-	if err != nil {
-		return cli.Exit(err.Error(), 1)
-	}
-	config.Debug = false
-
-	papi.Init(config)
-	hapi.Init(config)
-
 	var tfData TFData
 	tfData.EdgeHostnames = make(map[string]EdgeHostname)
 	tfData.Hostnames = make(map[string]Hostname)
@@ -167,15 +149,16 @@ func CmdCreateProperty(c *cli.Context) error {
 
 	// Get Property
 	propertyName := c.Args().First()
+	term := terminal.Get(ctx)
 	fmt.Println("Configuring Property")
 	term.Spinner().Start("Fetching property ")
-	property := findProperty(propertyName)
+	property := findProperty(ctx, client, propertyName)
 	if property == nil {
 		term.Spinner().Fail()
 		return cli.Exit(color.RedString("Property not found "), 1)
 	}
 
-	tfData.ContractID = property.Contract.ContractID
+	tfData.ContractID = property.ContractID
 	tfData.PropertyName = property.PropertyName
 	tfData.PropertyID = property.PropertyID
 	tfData.PropertyResourceName = strings.Replace(property.PropertyName, ".", "-", -1)
@@ -183,7 +166,13 @@ func CmdCreateProperty(c *cli.Context) error {
 
 	// Get Property Rules
 	term.Spinner().Start("Fetching property rules ")
-	rules, err := property.GetRules("")
+	rules, err := client.GetRuleTree(ctx, papi.GetRuleTreeRequest{
+		PropertyID:      property.PropertyID,
+		PropertyVersion: property.LatestVersion,
+		ContractID:      property.ContractID,
+		GroupID:         property.GroupID,
+		RuleFormat:      property.RuleFormat,
+	})
 
 	if err != nil {
 		term.Spinner().Fail()
@@ -191,11 +180,11 @@ func CmdCreateProperty(c *cli.Context) error {
 	}
 
 	tfData.IsSecure = "false"
-	if rules.Rule.Options.IsSecure {
+	if rules.Rules.Options.IsSecure {
 		tfData.IsSecure = "true"
 	}
 
-	for _, behaviour := range rules.Rule.Behaviors {
+	for _, behaviour := range rules.Rules.Behaviors {
 		_ = behaviour
 		if behaviour.Name == "cpCode" {
 			options := behaviour.Options
@@ -209,17 +198,17 @@ func CmdCreateProperty(c *cli.Context) error {
 
 	// Set up template structure
 	var ruletemplate RuleTemplate
-	ruletemplate.Name = rules.Rule.Name
-	ruletemplate.Criteria = rules.Rule.Criteria
-	ruletemplate.Behaviors = rules.Rule.Behaviors
-	ruletemplate.Comments = rules.Rule.Comments
-	ruletemplate.CriteriaLocked = rules.Rule.CriteriaLocked
-	ruletemplate.CriteriaMustSatisfy = rules.Rule.CriteriaMustSatisfy
-	ruletemplate.UUID = rules.Rule.UUID
-	ruletemplate.Variables = rules.Rule.Variables
-	ruletemplate.AdvancedOverride = rules.Rule.AdvancedOverride
+	ruletemplate.Name = rules.Rules.Name
+	ruletemplate.Criteria = rules.Rules.Criteria
+	ruletemplate.Behaviors = rules.Rules.Behaviors
+	ruletemplate.Comments = rules.Rules.Comments
+	ruletemplate.CriteriaLocked = rules.Rules.CriteriaLocked
+	ruletemplate.CriteriaMustSatisfy = rules.Rules.CriteriaMustSatisfy
+	ruletemplate.UUID = rules.Rules.UUID
+	ruletemplate.Variables = rules.Rules.Variables
+	ruletemplate.AdvancedOverride = rules.Rules.AdvancedOverride
 	ruletemplate.Children = make([]string, 0)
-	ruletemplate.Options = rules.Rule.Options
+	ruletemplate.Options = rules.Rules.Options
 
 	var rulestemplate RulesTemplate
 	rulestemplate.AccountID = rules.AccountID
@@ -235,7 +224,7 @@ func CmdCreateProperty(c *cli.Context) error {
 	snippetspath := filepath.Join(tools.TFWorkPath, "property-snippets")
 	os.Mkdir(snippetspath, 0755)
 
-	for _, rule := range rules.Rule.Children {
+	for _, rule := range rules.Rules.Children {
 		jsonBody, err := json.MarshalIndent(rule, "", "  ")
 		if err != nil {
 			term.Spinner().Fail()
@@ -268,7 +257,7 @@ func CmdCreateProperty(c *cli.Context) error {
 
 	// Get Group
 	term.Spinner().Start("Fetching group ")
-	group, err := getGroup(property.GroupID)
+	group, err := getGroup(ctx, client, property.GroupID)
 	if err != nil {
 		term.Spinner().Fail()
 		return cli.Exit(color.RedString("Group not found: %s", err), 1)
@@ -281,19 +270,19 @@ func CmdCreateProperty(c *cli.Context) error {
 
 	// Get Version
 	term.Spinner().Start("Fetching property version ")
-	version, err := getVersion(property)
+	version, err := getVersion(ctx, client, property)
 	if err != nil {
 		term.Spinner().Fail()
 		return cli.Exit(color.RedString("Version not found: %s", err), 1)
 	}
 
-	tfData.ProductID = version.ProductID
+	tfData.ProductID = version.Version.ProductID
 
 	term.Spinner().OK()
 
 	// Get Product
 	term.Spinner().Start("Fetching product name ")
-	product, err := getProduct(tfData.ProductID, property.Contract)
+	product, err := getProduct(ctx, client, tfData.ProductID, property.ContractID)
 	if err != nil {
 		term.Spinner().Fail()
 		return cli.Exit(color.RedString("Product not found: %s", err), 1)
@@ -305,14 +294,14 @@ func CmdCreateProperty(c *cli.Context) error {
 
 	// Get Hostnames
 	term.Spinner().Start("Fetching hostnames ")
-	hostnames, err := getHostnames(property, version)
+	hostnames, err := getHostnames(ctx, client, property, version)
 
 	if err != nil {
 		term.Spinner().Fail()
 		return cli.Exit(color.RedString("Hostnames not found: %s", err), 1)
 	}
 
-	for _, hostname := range hostnames.Hostnames.Items {
+	for _, hostname := range hostnames.Items {
 		_ = hostname
 
 		if hostname.EdgeHostnameID == "" {
@@ -320,9 +309,13 @@ func CmdCreateProperty(c *cli.Context) error {
 		}
 
 		// Get slot details
-		ehnid := strings.Replace(hostname.EdgeHostnameID, "ehn_", "", 1)
+		ehnid, err := strconv.Atoi(strings.Replace(hostname.EdgeHostnameID, "ehn_", "", 1))
+		if err != nil {
+			term.Spinner().Fail()
+			return cli.Exit(color.RedString("Invalid Hostname id: %s", err), 1)
+		}
 
-		edgehostname, err := hapi.GetEdgeHostnameByID(ehnid)
+		edgehostname, err := clientHapi.GetEdgeHostname(ctx, ehnid)
 		if err != nil {
 			term.Spinner().Fail()
 			return cli.Exit(color.RedString("Edge Hostname not found: %s", err), 1)
@@ -337,10 +330,10 @@ func CmdCreateProperty(c *cli.Context) error {
 		edgeHostnameN.EdgeHostnameID = hostname.EdgeHostnameID
 		edgeHostnameN.EdgeHostnameResourceName = cnameToResource
 		edgeHostnameN.ProductName = product.ProductName
-		edgeHostnameN.IPv6 = getIPv6(property, hostname.EdgeHostnameID)
+		edgeHostnameN.IPv6 = getIPv6(ctx, client, property, hostname.EdgeHostnameID)
 		edgeHostnameN.SlotNumber = edgehostname.SlotNumber
 		edgeHostnameN.SecurityType = edgehostname.SecurityType
-		edgeHostnameN.ContractID = property.Contract.ContractID
+		edgeHostnameN.ContractID = property.ContractID
 		edgeHostnameN.GroupID = group.GroupID
 		tfData.EdgeHostnames[cnameToResource] = edgeHostnameN
 
@@ -355,11 +348,15 @@ func CmdCreateProperty(c *cli.Context) error {
 
 	// Get contact details
 	term.Spinner().Start("Fetching contact details ")
-	activations, err := property.GetActivations()
+	activationsResponse, err := client.GetActivations(ctx, papi.GetActivationsRequest{
+		PropertyID: property.PropertyID,
+		ContractID: property.ContractID,
+		GroupID:    property.GroupID,
+	})
 	if err != nil {
 		tfData.Emails = append(tfData.Emails, "")
 	} else {
-		a, err := activations.GetLatestStagingActivation("")
+		a, err := getLatestStagingActivation(activationsResponse.Activations, "")
 		if err != nil {
 			tfData.Emails = append(tfData.Emails, "")
 		} else {
@@ -384,64 +381,92 @@ func CmdCreateProperty(c *cli.Context) error {
 
 }
 
-func getHostnames(property *papi.Property, version *papi.Version) (*papi.Hostnames, error) {
-	hostnames, err := property.GetHostnames(version, "")
+func getHostnames(ctx context.Context, client papi.PAPI, property *papi.Property, version *papi.GetPropertyVersionsResponse) (*papi.HostnameResponseItems, error) {
+	if version == nil {
+		var err error
+		version, err = client.GetLatestVersion(ctx, papi.GetLatestVersionRequest{
+			PropertyID:  property.PropertyID,
+			ActivatedOn: "",
+			ContractID:  property.ContractID,
+			GroupID:     property.GroupID,
+		})
+		if err != nil {
+			return nil, err
+		}
+	}
+	response, err := client.GetPropertyVersionHostnames(ctx, papi.GetPropertyVersionHostnamesRequest{
+		PropertyID:        property.PropertyID,
+		PropertyVersion:   version.Version.PropertyVersion,
+		ContractID:        property.ContractID,
+		GroupID:           property.GroupID,
+		ValidateHostnames: false,
+		IncludeCertStatus: false,
+	})
 	if err != nil {
 		return nil, err
 	}
-	return hostnames, nil
+	return &response.Hostnames, nil
 }
 
-func getIPv6(property *papi.Property, ehn string) string {
-	edgeHostnames, err := papi.GetEdgeHostnames(property.Contract, property.Group, "")
+func getIPv6(ctx context.Context, client papi.PAPI, property *papi.Property, ehn string) string {
+	edgeHostnames, err := client.GetEdgeHostnames(ctx, papi.GetEdgeHostnamesRequest{
+		ContractID: property.ContractID,
+		GroupID:    property.GroupID,
+		Options:    nil,
+	})
 	if err != nil {
 		return "false"
 	}
 	for _, edgehostname := range edgeHostnames.EdgeHostnames.Items {
 		_ = edgehostname
-		if edgehostname.EdgeHostnameID == ehn {
+		if edgehostname.ID == ehn {
 			return edgehostname.IPVersionBehavior
 		}
 	}
 	return ""
 }
 
-func findProperty(name string) *papi.Property {
-	results, err := papi.Search(papi.SearchByPropertyName, name, "")
+func findProperty(ctx context.Context, client papi.PAPI, name string) *papi.Property {
+	results, err := client.SearchProperties(ctx, papi.SearchRequest{
+		Key:   papi.SearchKeyPropertyName,
+		Value: name,
+	})
 	if err != nil {
 		return nil
 	}
 
-	if err != nil || results == nil {
+	if err != nil || results == nil || len(results.Versions.Items) == 0 {
 		return nil
 	}
 
-	property := &papi.Property{
+	response, err := client.GetProperty(ctx, papi.GetPropertyRequest{
 		PropertyID: results.Versions.Items[0].PropertyID,
-		Group: &papi.Group{
-			GroupID: results.Versions.Items[0].GroupID,
-		},
-		Contract: &papi.Contract{
-			ContractID: results.Versions.Items[0].ContractID,
-		},
-	}
-
-	err = property.GetProperty("")
+		GroupID:    results.Versions.Items[0].GroupID,
+		ContractID: results.Versions.Items[0].ContractID,
+	})
 	if err != nil {
 		return nil
 	}
 
-	return property
+	return response.Property
 }
 
-func getVersion(property *papi.Property) (*papi.Version, error) {
-
-	versions, err := property.GetVersions("")
+func getVersion(ctx context.Context, client papi.PAPI, property *papi.Property) (*papi.GetPropertyVersionsResponse, error) {
+	versions, err := client.GetPropertyVersions(ctx, papi.GetPropertyVersionsRequest{
+		PropertyID: property.PropertyID,
+		ContractID: property.ContractID,
+		GroupID:    property.GroupID,
+	})
 	if err != nil {
 		return nil, err
 	}
 
-	version, err := versions.GetLatestVersion("", "")
+	version, err := client.GetLatestVersion(ctx, papi.GetLatestVersionRequest{
+		PropertyID:  versions.PropertyID,
+		ActivatedOn: "",
+		ContractID:  versions.ContractID,
+		GroupID:     versions.GroupID,
+	})
 	if err != nil {
 		return nil, err
 	}
@@ -449,14 +474,13 @@ func getVersion(property *papi.Property) (*papi.Version, error) {
 	return version, nil
 }
 
-func getGroup(groupID string) (*papi.Group, error) {
-	groups := papi.NewGroups()
-	e := groups.GetGroups("")
-	if e != nil {
-		return nil, e
+func getGroup(ctx context.Context, client papi.PAPI, groupID string) (*papi.Group, error) {
+	groups, err := client.GetGroups(ctx)
+	if err != nil {
+		return nil, err
 	}
 
-	group, e := groups.FindGroup(groupID)
+	group, e := findGroup(groups.Groups, groupID)
 	if e != nil {
 		return nil, e
 	}
@@ -464,23 +488,101 @@ func getGroup(groupID string) (*papi.Group, error) {
 	return group, nil
 }
 
-func getProduct(productID string, contract *papi.Contract) (*papi.Product, error) {
-	if contract == nil {
+// FindGroup finds a specific group by ID
+func findGroup(groups papi.GroupItems, id string) (*papi.Group, error) {
+	var group *papi.Group
+	var groupFound bool
+
+	if id == "" {
+		goto err
+	}
+
+	for _, group = range groups.Items {
+		if group.GroupID == id {
+			groupFound = true
+			break
+		}
+	}
+
+err:
+	if !groupFound {
+		return nil, fmt.Errorf("unable to find group: \"%s\"", id)
+	}
+
+	return group, nil
+}
+
+func getProduct(ctx context.Context, client papi.PAPI, productID string, contractID string) (*papi.ProductItem, error) {
+	if contractID == "" {
 		return nil, nil
 	}
 
-	products := papi.NewProducts()
-	e := products.GetProducts(contract, "")
-	if e != nil {
-		return nil, e
+	products, err := client.GetProducts(ctx, papi.GetProductsRequest{
+		ContractID: contractID,
+	})
+	if err != nil {
+		return nil, err
 	}
 
-	product, e := products.FindProduct(productID)
+	product, e := findProduct(products, productID)
 	if e != nil {
 		return nil, e
 	}
 
 	return product, nil
+}
+
+// findProduct finds a specific product by ID
+func findProduct(products *papi.GetProductsResponse, id string) (*papi.ProductItem, error) {
+	var product papi.ProductItem
+	var productFound bool
+	for _, product = range products.Products.Items {
+		if product.ProductID == id {
+			productFound = true
+			break
+		}
+	}
+
+	if !productFound {
+		return nil, fmt.Errorf("unable to find product: \"%s\"", id)
+	}
+
+	return &product, nil
+}
+
+// getLatestStagingActivation retrieves the latest activation for the staging network
+//
+// Pass in a status to check for, defaults to StatusActive
+func getLatestStagingActivation(activations papi.ActivationsItems, status papi.ActivationStatus) (*papi.Activation, error) {
+	return getLatestActivation(activations, papi.ActivationNetworkStaging, status)
+}
+
+// getLatestActivation gets the latest activation for the specified network
+//
+// Defaults to NetworkProduction. Pass in a status to check for, defaults to StatusActive
+//
+// This can return an activation OR a deactivation. Check activation.ActivationType and activation.Status for what you're looking for
+func getLatestActivation(activations papi.ActivationsItems, network papi.ActivationNetwork, status papi.ActivationStatus) (*papi.Activation, error) {
+	if network == "" {
+		network = papi.ActivationNetworkProduction
+	}
+
+	if status == "" {
+		status = papi.ActivationStatusActive
+	}
+
+	var latest *papi.Activation
+	for _, activation := range activations.Items {
+		if activation.Network == network && activation.Status == status && (latest == nil || activation.PropertyVersion > latest.PropertyVersion) {
+			latest = activation
+		}
+	}
+
+	if latest == nil {
+		return nil, fmt.Errorf("no activation found (network: %s, status: %s)", network, status)
+	}
+
+	return latest, nil
 }
 
 func saveTerraformDefinition(data TFData) error {
