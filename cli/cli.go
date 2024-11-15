3fdef3de4a44aa3986c28e83c8c6d841e857068d
Malgorzata Niemczyk <mniemczy@akamai.com>
2024-11-15 11:56:17 +0000

    DXISSUE-3079 Add info on additional command flags for CAM in readme
    Merge in DEVEXP/cli-terraform from feature/DXISSUE-3079-cam-export-import-doc to develop

--- cli/cli.go
+++ cli/cli.go
@@ -1,18 +1,4 @@
-// Copyright 2019. Akamai Technologies, Inc
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// Package cli contains code for managing command line interface functionality
+// Package cli contains code for managing the command line interface functionality.
 package cli
 
 import (
@@ -44,7 +30,7 @@ func Run() error {
 
 	app := akacli.CreateAppTemplate(ctx, "terraform",
 		"A CLI Plugin for Akamai Terraform Provider",
-		"Administer and Manage Supported Akamai Feature resources with Terraform",
+		"Export selected resources for faster adoption in Terraform.",
 		Version)
 
 	cmds, err := commands.CommandLocator()
