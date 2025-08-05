425c4a5d95de3a68dcde95e4a43da567a91e7955
Piotr Bartosik <pbartosi@akamai.com>
2025-08-05 11:26:30 +0000

    DXE-5052 Remove empty line from CA set import template

--- pkg/providers/mtlstruststore/testdata/funny_ca_set_name/import.sh
+++ pkg/providers/mtlstruststore/testdata/funny_ca_set_name/import.sh
@@ -1,5 +1,4 @@
 terraform init
-
 terraform import akamai_mtlstruststore_ca_set.a_funny-set_name "12345"
 terraform import akamai_mtlstruststore_ca_set_activation.a_funny-set_name-staging "12345:STAGING"
 terraform import akamai_mtlstruststore_ca_set_activation.a_funny-set_name-production "12345:PRODUCTION"
