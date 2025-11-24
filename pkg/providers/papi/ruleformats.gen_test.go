bf3ddca4f3a3586963a32237284e36d08ebbf688
Piotr Bartosik <pbartosi@akamai.com>
2025-11-24 14:35:00 +0000

    DXE-5959 Add missing test for v2025-10-16

--- pkg/providers/papi/ruleformats.gen_test.go
+++ pkg/providers/papi/ruleformats.gen_test.go
@@ -362,6 +362,26 @@ func TestProcessPolicyTemplatesGenerated(t *testing.T) {
 			filesToCheck: []string{"property.tf", "rules.tf", "variables.tf", "import.sh"},
 			filterFuncs:  []func([]string) ([]string, error){useThisOnlyRuleFormat("v2025-09-09")},
 		},
+		"property with rules as datasource - hcl rules version v2025-10-16": {
+			givenData: TFData{
+				Property: TFPropertyData{
+					GroupName:            "test_group",
+					GroupID:              "grp_12345",
+					ContractID:           "test_contract",
+					PropertyResourceName: "test-edgesuite-net",
+					PropertyName:         "test.edgesuite.net",
+					PropertyID:           "prp_12345",
+					ProductID:            "prd_HTTP_Content_Del",
+					ProductName:          "HTTP_Content_Del",
+					RuleFormat:           "v2025-10-16",
+					IsSecure:             "false",
+					ReadVersion:          "LATEST",
+				},
+			},
+			dir:          "ruleformats/v2025_10_16/rules_datasource",
+			filesToCheck: []string{"property.tf", "rules.tf", "variables.tf", "import.sh"},
+			filterFuncs:  []func([]string) ([]string, error){useThisOnlyRuleFormat("v2025-10-16")},
+		},
 	}
 
 	for name, test := range tests {
